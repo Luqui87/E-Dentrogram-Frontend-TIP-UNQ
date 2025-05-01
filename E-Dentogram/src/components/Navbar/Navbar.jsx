4a16b816d6325534552bef08dfa618b5f9418611
JuanIgnGarcia <juan.ignacio.garcia.02@gmail.com>
2025-05-01 20:37:09 -0300

    logout-button

--- E-Dentogram/src/components/Navbar/Navbar.jsx
+++ E-Dentogram/src/components/Navbar/Navbar.jsx
@@ -4,17 +4,24 @@ import { useNavigate } from "react-router-dom";
 
 function Navbar() {
   const navigate = useNavigate();
-
   const token = localStorage.getItem("token");
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/");
+  };
 
   return (
     <>
       {token && (
         <header>
-          <div onClick={() => navigate("/Home")}>
+          <div onClick={() => navigate("/Home")} className="logo-section">
             <img src={logo} alt="Logo de aplicación" />
             <a>E-Dentogram</a>
           </div>
+          <button className="logout-button" onClick={handleLogout}>
+            Logout
+          </button>
         </header>
       )}
     </>
