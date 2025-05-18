4db0bcde479d21fb4564130e394e64a0ee62bf92
Lucas Alvarez <alvarezlucas2787@gmail.com>
2025-05-18 20:28:46 -0300

    Incorporación de FullCalendar

--- E-Dentogram/src/components/Calendario/Calendario.jsx
+++ E-Dentogram/src/components/Calendario/Calendario.jsx
@@ -1,11 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { gapi } from 'gapi-script';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import esLocale from '@fullcalendar/core/locales/es';
+import './Calendario.css'
 
 const CLIENT_ID = '1042049294933-6706691g5vb2fgonludemk973v9mlgeb.apps.googleusercontent.com';
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
 const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
+
+
 function CalendarApp() {
+
+  const [events, setEvents] = useState([])
+
   useEffect(() => {
     const initClient = () => {
       gapi.client
@@ -34,16 +44,51 @@ function CalendarApp() {
         timeMin: new Date().toISOString(),
         showDeleted: false,
         singleEvents: true,
-        maxResults: 10,
+        maxResults: 100,
         orderBy: 'startTime',
       })
       .then(response => {
-        const events = response.result.items;
-        console.log('Upcoming events:', events);
+        const getEvents = response.result.items.map((d) => ({title: d.summary , start:d.start.dateTime, end:d.end.dateTime }));
+        setEvents(getEvents)
       });
   };
 
-  return <div></div>;
+  
+
+  return (
+    <main>
+      <div className="calendario">
+        <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        initialView="timeGridWeek"
+        locale={esLocale}
+        height={'100%'}
+        headerToolbar={{
+          left: 'prev,next today',
+          center: 'title',
+          right: 'dayGridMonth,timeGridWeek'
+        }}
+        views={{
+          dayGridMonth: { buttonText: 'Mes' },
+          timeGridWeek: { buttonText: 'Semana' }
+        }}
+        events={events}
+      />
+      </div>
+    
+    </main>
+  )
 }
 
+
+// a custom render function
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  )
+}
 export default CalendarApp;
+
