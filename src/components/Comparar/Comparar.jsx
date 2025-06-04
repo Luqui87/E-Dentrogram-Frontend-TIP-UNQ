829fed6b8f295bc6edbdea124078a9491e012ff6
Lucas Alvarez <alvarezlucas2787@gmail.com>
2025-06-04 14:22:40 -0300

    Estilo de comparar

--- src/components/Comparar/Comparar.jsx
+++ src/components/Comparar/Comparar.jsx
@@ -1,16 +1,46 @@
+import { useState } from 'react'
 import Modal from '../Modal'
 import Odontograma from '../Odontograma/Odontograma'
 
+
+const mockedTeeth = [
+        {
+        up:"MISSING",
+        left:"MISSING",
+        center:"MISSING",
+        right:"MISSING",
+        down:"MISSING",
+        special: "NOTHING",
+        number: 1
+        },
+        {
+        up:"HEALTHY",
+        left:"HEALTHY",
+        center:"HEALTHY",
+        right:"HEALTHY",
+        down:"HEALTHY",
+        special: "DENTAL_CROWNS_WITH_ROOT_CANAL_TREATMENT",
+        number: 52
+        },
+    ]
+
 function Comparar(){
+
+    const [type,setType] = useState("Adulto")
 
     return(
         <Modal isOpen={true} >
+
+            <select value={type} onChange={e => setType(e.target.value)} className="classic" style={{right:"1px"}}>
+                <option>Adulto</option>
+                <option>Infante</option>
+          </select>
             <h2 style={{margin:"auto"}}>Version en la fecha 20/05</h2>
-            <Odontograma type={"Adulto"} active={'active'} teeth={[]} />
+            <Odontograma type={"Infante Comparar"} active={'active'} teeth={mockedTeeth} />
             <hr style={{height: "5px", backgroundColor: "black"}} />
 
             <h2>Version en la fecha 05/06 </h2>
-            <Odontograma type={"Adulto"} active={'active'} teeth={[]} />
+            <Odontograma type={"Adulto Comparar"} active={'active'} teeth={mockedTeeth} />
         </Modal>
     )
 }
