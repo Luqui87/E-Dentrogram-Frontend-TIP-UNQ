0c8e57f1b8b819a3db5965552410c05bdb1db6f2
JuanIgnGarcia <juan.ignacio.garcia.02@gmail.com>
2025-06-27 23:29:04 -0300

    Fixes

--- src/components/PatientsLogs/PatientLogs.jsx
+++ src/components/PatientsLogs/PatientLogs.jsx
@@ -1,121 +1,149 @@
-import './PatientLogs.css'
-import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css';
-import AddPatientLogModal from './AddPatientLogModal';
-import { useEffect, useState } from 'react';
-import API, { handleApiError } from '../../service/API';
+import "./PatientLogs.css";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import AddPatientLogModal from "./AddPatientLogModal";
+import { useEffect, useState } from "react";
+import API, { handleApiError } from "../../service/API";
 
+function PatientLogs({ active, id }) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [patientLogs, setPatientLogs] = useState([]);
+  const [page, setPage] = useState(0);
 
+  useEffect(() => {
+    API.getPatientJournal(id, page)
+      .then((res) => {
+        setPatientLogs(res.data.journal);
+      })
+      .catch((error) => {
+        handleApiError(error);
+      });
+  }, []);
 
-function PatientLogs({active, id}){
+  const contentStyle = {
+    marginLeft: "140px",
+    width: "84%",
+  };
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [patientLogs, setPatientLogs] = useState([]);
-    const [page, setPage] = useState(0);
+  const AddIcon = () => {
+    return (
+      <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+          id="SVGRepo_tracerCarrier"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+          {" "}
+          <path
+            d="M7 12L12 12M12 12L17 12M12 12V7M12 12L12 17"
+            stroke="#ffffff"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          ></path>{" "}
+        </g>
+      </svg>
+    );
+  };
 
-    useEffect(() => {
-        API.getPatientJournal(id,page)
-            .then((res) => {
-                setPatientLogs(res.data.journal);
-            })
-            .catch((error) => {
-                handleApiError(error)
-            })
-    }, []);
+  const logs = patientLogs.map((log, index) => {
+    const date = new Date(log.date);
+    const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
 
-    const contentStyle = {
-        marginLeft:"140px",
-        width:"84%"
-    };
+    const day = date.getDate();
+    const month = months[date.getMonth()].toLowerCase();
+    const formattedDate = `${day} de ${month}`;
 
-    const AddIcon = () => {
-        return (
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M7 12L12 12M12 12L17 12M12 12V7M12 12L12 17" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-        );
-    };
-
-
-    const logs = patientLogs.map((log, index) => {
-        
-        const date = new Date(log.date);
-        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-
-        const day = date.getDate();
-        const month = months[date.getMonth()].toLowerCase();
-        const formattedDate = `${day} de ${month}`;
-
-        return(
-        <VerticalTimelineElement
-            className="patient-log"
-            contentStyle= {contentStyle}
-            contentArrowStyle={{ display:'none' }}
-            date={formattedDate}
-            iconStyle={{ background: "#95B6BD", boxShadow:"95B6BD"}}
-            shadowSize="medium"
-            iconClassName="patient-log-icon"
-            dateClassName='patient-log-date'
-        >
-            <p>
-               {log.log}
-            </p>
-            <div className="tags">
-                {log.tags.map((tag, index) => <span key={index} className="tag">{tag}</span>)}
-            </div>
-        </VerticalTimelineElement>
-        )
-    })
-
-    const handleAddLog = (log) => {
-        setPatientLogs([log, ...patientLogs]);
-    }
-
-    const handleGetMoreEntries = () => {
-        API.getPatientJournal(id, (page + 1))
-            .then((res) => {
-                setPage(page + 1 );
-                setPatientLogs([res.data.journal , ...patientLogs]);
-            })
-            .catch((error) => {
-                handleApiError(error)
-            })
-    }
-
-
-    return(
-        <div className={`patient-logs ${active} `}>
-            <VerticalTimeline
-                animate={false}
-                layout='1-column-left'
-                
-            >
-                <VerticalTimelineElement
-                    className="patient-log"
-                    contentStyle= {contentStyle}
-                    contentArrowStyle={{ display:'none' }}
-                    iconStyle={{ background: "#95B6BD",}}
-                    iconClassName="patient-log-add"
-                    shadowSize="medium"
-                    icon={<AddIcon/>}
-                    iconOnClick={() => setIsModalOpen(true)}
-                >
-                </VerticalTimelineElement>
-                
-                {logs}
-
-                <VerticalTimelineElement
-                    className="patient-log"
-                    iconStyle={{ background: "#035E7B",}}
-                    shadowSize="medium"
-                    iconClassName="patient-log-add"
-                    icon={<AddIcon/>}
-                    iconOnClick={handleGetMoreEntries}
-                ></VerticalTimelineElement>
-
-            </VerticalTimeline>
-
-            <AddPatientLogModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} handleAddLog={handleAddLog} id={id}/>
+    return (
+      <VerticalTimelineElement
+        className="patient-log"
+        contentStyle={contentStyle}
+        contentArrowStyle={{ display: "none" }}
+        date={formattedDate}
+        iconStyle={{ background: "#95B6BD", boxShadow: "95B6BD" }}
+        shadowSize="medium"
+        iconClassName="patient-log-icon"
+        dateClassName="patient-log-date"
+      >
+        <p>{log.log}</p>
+        <div className="tags">
+          {log.tags.map((tag, index) => (
+            <span key={index} className="tag">
+              {tag}
+            </span>
+          ))}
         </div>
-    )
+      </VerticalTimelineElement>
+    );
+  });
+
+  const handleAddLog = (log) => {
+    setPatientLogs([log, ...patientLogs]);
+  };
+
+  const handleGetMoreEntries = () => {
+    API.getPatientJournal(id, page + 1)
+      .then((res) => {
+        setPage(page + 1);
+        setPatientLogs(patientLogs.concat(res.data.journal));
+      })
+      .catch((error) => {
+        handleApiError(error);
+      });
+  };
+
+  return (
+    <div className={`patient-logs ${active} `}>
+      <VerticalTimeline animate={false} layout="1-column-left">
+        <VerticalTimelineElement
+          className="patient-log"
+          contentStyle={contentStyle}
+          contentArrowStyle={{ display: "none" }}
+          iconStyle={{ background: "#95B6BD" }}
+          iconClassName="patient-log-add"
+          shadowSize="medium"
+          icon={<AddIcon />}
+          iconOnClick={() => setIsModalOpen(true)}
+        ></VerticalTimelineElement>
+
+        {logs}
+
+        <VerticalTimelineElement
+          className="patient-log"
+          iconStyle={{ background: "#035E7B" }}
+          shadowSize="medium"
+          iconClassName="patient-log-add"
+          icon={<AddIcon />}
+          iconOnClick={handleGetMoreEntries}
+        ></VerticalTimelineElement>
+      </VerticalTimeline>
+
+      <AddPatientLogModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        handleAddLog={handleAddLog}
+        id={id}
+      />
+    </div>
+  );
 }
 
-export default PatientLogs
+export default PatientLogs;
--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,89 +1,107 @@
-import { useEffect, useState } from 'react';
-import Tag from '../../components/PatientsLogs/Tag'
-import './Settings.css'
-import API, { handleApiError } from '../../service/API';
-import { toast } from 'react-toastify';
+import { useEffect, useState } from "react";
+import Tag from "../../components/PatientsLogs/Tag";
+import "./Settings.css";
+import API, { handleApiError } from "../../service/API";
+import { toast } from "react-toastify";
 
-function Settings(){
+function Settings() {
+  const [inputValue, setInputValue] = useState("");
+  const [tags, setTags] = useState([]);
+  const [selected, setSelected] = useState("Tags");
 
-    const [inputValue, setInputValue] = useState('');
-    const [tags, setTags] = useState([]);
-    const [selected, setSelected] = useState("Tags")
+  useEffect(() => {
+    getTagsFromStorage();
+  }, []);
 
-    
-    useEffect(() => {
-        getTagsFromStorage();
+  const getTagsFromStorage = () => {
+    const userTags = JSON.parse(localStorage.getItem("userTags"));
+    setTags(userTags);
+  };
 
-    }, []);
-
-    const getTagsFromStorage = () => {
-        const userTags = JSON.parse(localStorage.getItem('userTags'));
-        setTags( ["tag", "tag", "tag"] );
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      setTags([...tags, inputValue]);
+      setInputValue("");
     }
+  };
 
-    const handleKeyDown = (event) => {
-        if (event.key === 'Enter') {
-          setTags([...tags, inputValue]);
-          setInputValue('')
-        }
-      };
-      
-    const handleNewTags = () => {
-        API.patchDentistTags(tags)
-        .then((res) => {
-            localStorage.setItem('userTags',JSON.stringify(res.data.tags));
-            toast.success("Etiquetas modificadas");
-        })
-        .catch((error) =>{
-            handleApiError(error);
-        } )
-    }
+  const handleNewTags = () => {
+    API.patchDentistTags(tags)
+      .then((res) => {
+        localStorage.setItem("userTags", JSON.stringify(res.data.tags));
+        toast.success("Etiquetas modificadas");
+      })
+      .catch((error) => {
+        handleApiError(error);
+      });
+  };
 
-    const mapTags = tags.map((tag,index) => <Tag key={index} setTag={()=>{}}>{tag}</Tag>);
+  const mapTags = tags.map((tag, index) => (
+    <Tag key={index} setTag={() => {}}>
+      {tag}
+    </Tag>
+  ));
 
-    return(
-        <main>
-            <div className="settings">
-                <div className="left-bar">
-                    <h2 style={{textAlign:"center"}}>Configuración</h2>
-                    <div className="bar-items">
-                        <span className={selected == "Tags"? "active" : ""} onClick={()=>setSelected("Tags")}>Etiquetas</span>
-                        <span className={selected == "PreTurn"? "active" : ""} onClick={()=>setSelected("PreTurn")}>Mensaje pre-turno</span>
-                    </div>
-                </div>
-                <div className="setting-content">
-                    <div className="settings-tags" style={{display: selected=="Tags"? "" : "none"}}>
-                        <h2>Configurar Etiquetas</h2>
-                        <label htmlFor="new-tag">
-                            <input type="text" name="new-tag" id="new-tag"
-                             placeholder='Ingresar etiqueta nueva'
-                            value={inputValue}
-                            onChange={(e) => setInputValue(e.target.value)}
-                            onKeyDown={handleKeyDown} />
-                        </label>
-                        
-                        <span>Etiquetas actuales</span>
-                        <div className="use-tags">
-                            <div className="tags">
-                                {mapTags}
-                            </div>
-                            
-                        </div>
+  return (
+    <main>
+      <div className="settings">
+        <div className="left-bar">
+          <h2 style={{ textAlign: "center" }}>Configuración</h2>
+          <div className="bar-items">
+            <span
+              className={selected == "Tags" ? "active" : ""}
+              onClick={() => setSelected("Tags")}
+            >
+              Etiquetas
+            </span>
+            {
+              //<span className={selected == "PreTurn"? "active" : ""} onClick={()=>setSelected("PreTurn")}>Mensaje pre-turno</span>
+            }
+          </div>
+        </div>
+        <div className="setting-content">
+          <div
+            className="settings-tags"
+            style={{ display: selected == "Tags" ? "" : "none" }}
+          >
+            <h2>Configurar Etiquetas</h2>
+            <label htmlFor="new-tag">
+              <input
+                type="text"
+                name="new-tag"
+                id="new-tag"
+                placeholder="Ingresar etiqueta nueva"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+              />
+            </label>
 
-                        <div className="buttons">
-                            <button className="btn-confirmar" onClick={() => handleNewTags()}>
-                            Confirmar
-                            </button>
-                            <button className="btn-cancelar" onClick={() => getTagsFromStorage()}>
-                            Cancelar
-                            </button>
-                        </div>
-                    </div>
-                    <div className="settings-preturn" style={{display: selected=="PreTrun"? "" : "none"}}></div>
-                </div>
+            <span>Etiquetas actuales</span>
+            <div className="use-tags">
+              <div className="tags">{mapTags}</div>
             </div>
-        </main>
-    )
+
+            <div className="buttons">
+              <button className="btn-confirmar" onClick={() => handleNewTags()}>
+                Confirmar
+              </button>
+              <button
+                className="btn-cancelar"
+                onClick={() => getTagsFromStorage()}
+              >
+                Cancelar
+              </button>
+            </div>
+          </div>
+          <div
+            className="settings-preturn"
+            style={{ display: selected == "PreTrun" ? "" : "none" }}
+          ></div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default Settings
+export default Settings;
